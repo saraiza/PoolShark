a3413412de5ab97a5c8f74e963caaaf70676aa8f
Steve Araiza <saraiza@contractor.net>
2021-11-24 18:14:27 -0500

    pipeline start

--- Scripts/findtable.py
+++ Scripts/findtable.py
@@ -5,7 +5,7 @@ import cv2 as cv
 def ShowImg(title, img):
     dy = img.shape[0]
     dx = img.shape[1]
-    maxshow = 400
+    maxshow = 800
     if dy > dx:
         scalefactor = maxshow / img.shape[0] #y
     else:
@@ -14,13 +14,16 @@ def ShowImg(title, img):
     dx = int(scalefactor * img.shape[1])
     dy = int(scalefactor * img.shape[0])
     siz = dx,dy
-    #imgsm = cv.resize(img,dsize=(dx,dy),fx=scalefactor, fy=scalefactor, interpolation = cv.INTER_AREA)
     imgsm = cv.resize(img,dsize=siz, fx=scalefactor, fy=scalefactor, interpolation = cv.INTER_AREA)
     cv.imshow(title, imgsm)
 
 img = cv.imread('C:/dev/PoolShark.a/test/images/IMG_4430.jpg')
-
-
 ShowImg('orig', img)
+
+blur = cv.GaussianBlur(img, (5,5), cv.BORDER_DEFAULT)
+ShowImg('blur',blur)
+
+canny = cv.Canny(img, 100, 175)
+ShowImg('canny',canny)
 
 cv.waitKey(0) 
