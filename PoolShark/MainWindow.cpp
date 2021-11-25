133b87f8de0a3d52672e2af078c3e955a8228522
Steve Araiza <saraiza@contractor.net>
2021-11-24 19:15:34 -0500

    working OpenCV integration

--- PoolShark/MainWindow.cpp
+++ PoolShark/MainWindow.cpp
@@ -1,8 +1,21 @@
-#include "MainWindow.h"
 #include "stdafx.h"
+#include "MainWindow.h"
+#include <iostream>                        // std::cout
+#include <opencv2/core/core.hpp>           // cv::Mat
+#include <opencv2/imgcodecs/imgcodecs.hpp>     // cv::imread()
+#include <opencv2/imgproc/imgproc.hpp>     // cv::Canny()
+#include <opencv2/highgui.hpp>
+
+using namespace cv;
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+
+    // Experiment with OpenCV
+    QString sFilename = "C:/dev/PoolShark.a/test/images/IMG_4430.jpg";
+    Mat img;
+    img = imread(qPrintable(sFilename));
+    imshow("img", img);
 }
