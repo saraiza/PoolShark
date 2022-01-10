7c063324dce75ac7c37fc574942d9efa2c5be7dd
Steve Araiza <saraiza@contractor.net>
2022-01-09 19:41:41 -0500

    more flexibility for images and markers

---
+++ test/images/long_all/markers.py
@@ -0,0 +1,31 @@
+# Marker hints for the image
+markers_long_all = [
+    (1098,342), # top
+    (1801,331),
+    
+    (558,660), #left
+    (501,924),
+    (430,1228),
+    (270,1993),
+    (176,2474),
+    (54,3043),
+    
+    (2350,636), #right
+    (2414,900),
+    (2479,1198),
+    (2654,1956),
+    (2763,2437),
+    (2895,2992),
+    
+    (799,3991), #bottom
+    (1493,3981),
+    (2187,3981)
+]
+
+
+def GetMarkers():
+    return markers_long_all
+
+print("markers.py loaded and executed")
+
+markers = markers_long_all
